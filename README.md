02bada7bb68eaa50fbacce9bb7c69e2e0dd0bc24
Mark Foyster <mxfoyster@yahoo.co.uk>
2022-04-03 12:41:52 +0100

    Colour picker and some other small changes

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ A simple graphics calculator app based on JavaScript and graph.js
 
 This will be a simple background project inspired by the likes of [https://www.desmos.com/calculator](https://www.desmos.com/calculator).
 
-For now, we can just set the range and grid size / resolution of the x axis and choose between two pre-defined functions (sin and x^2). It's something to get us started.
+For now, we can just set the range and grid size / resolution of the x axis and choose between two pre-defined functions (sin and x^2). 
+
+Most recent addition is a colour opicker for the graph line and now the graph automatically updates when we change ANY value. The css has been tweaked slightly to make things a little tidier.
 
 Keep your eyes on this, I've only just started!
 
--- js/mainChart.js
+++ js/mainChart.js
@@ -2,18 +2,17 @@
 const xGrid = document.getElementById("xGrid");
 const xMin = document.getElementById("xMin");
 const xMax = document.getElementById("xMax");
+const lineColor = document.getElementById("lineColor");
 
 graphFunction = "Math.sin(x)";
 chartLabel = "Chart of SIN(x)";
 
 document.getElementById("sin").addEventListener("click", sin);
 document.getElementById("square").addEventListener("click", square);
-
-//const xAxis = [];
-//const yAxis = [];
-
 var xValues = [];
 var yValues = [];
+
+
 generateData(graphFunction, 0, 10, 0.5); //our starting chart data
 plotChart();
 
@@ -31,13 +30,11 @@ function square()
 	rePlot();
 }
 
-
 //This is what we call to re-plot our chart when user prompts us to
 function rePlot()
 {
 	generateData(graphFunction, Number(xMin.value), Number(xMax.value), Number(xGrid.value));
 	plotChart();
-	
 }
 
 //this is the plotting code
@@ -49,7 +46,7 @@ function plotChart()
 		data: 
 		{
 			labels: xValues,
-			datasets: [{ fill: false, pointRadius: 1, borderColor: "rgba(255,0,0,0.5)", data: yValues}]
+			datasets: [{ fill: false, pointRadius: 1, borderColor: hexToNum() , data: yValues}]
 		},    
 		options: 
 		{legend: {display: false}, title: {display: true, text: chartLabel, fontSize: 16}}
@@ -69,3 +66,17 @@ function generateData(value, i1, i2, step = 1)
     xValues.push(x);
   }
 }
+
+//gets the values from the colour picker and builds them into an rgba value for chart.js
+function hexToNum()
+  {
+	  var lineColorDec = [];
+	  for (i=0; i<3; i++)//for each colour channel
+	  {
+		lineColorDec[i] = lineColor.value.slice((i+i+1),(i+i+3));  //cut out the this channel
+		lineColorDec[i] = parseInt(lineColorDec[i],16);//convert from hex to decimal
+	  }
+	  //build the string
+	  var rgbaString = "rgba(" + lineColorDec[0] + "," +  lineColorDec[1] + "," + lineColorDec[2] +",1)";
+	  return rgbaString;//return it
+  }
